106cab2006f33283d08b32451953a0d5c1544615
The fuzzy git user on PC 2 <fuzzygit@fuzzyenterprises.com>
2019-11-25 12:21:52 +0100

    Übungen w3resources

---
+++ Übung1_w3resources.js
@@ -0,0 +1,11 @@
+function js_style() {
+    text.style.fontSize = "14pt";
+    text.style.fontFamily = "Comic Sans MS";
+    text.style.color = "white";
+    text.style.background = "#f480ff";
+    text.style.padding = "20px";
+    text.style.margin = "20px";
+    text.style.borderWidth = "5px";
+    text.style.borderStyle = "solid";
+    text.style.borderColor = "#5c204fa2";
+}
