da9394819155f5657415f927351b81afa531e10e
The fuzzy git user on PC 2 <fuzzygit@fuzzyenterprises.com>
2020-01-15 16:46:38 +0100

    finale Änderungen

--- WebApp_MOBSProjekt_V4.js
+++ WebApp_MOBSProjekt_V4.js
@@ -47,78 +47,6 @@ function AnzeigeUhr() {
 
 window.onload = AnzeigeUhr;
 
-
-/*ANFANG AUSKOMMENTIERT
-
-//Pi einbinden: Statusinfos
-"use Strict";       //Code wird pingelig durchgegangen --> Fehler fallen eher auf, da HTML und JS sonst sehr verzeilich sind
-fetch("http://192.168.178.82:5000/status").then(function (response) {         //192.168.0.82 im Labor!!
-    response.text().then(function (text) {
-        console.log(text);
-        var array = text.split(',');
-        var consumption = array[0].split(":");
-        var humidity = array[1].substr(15, 6);
-        var pressure = array[2].substr(15, 6);
-        var speed = array[3].split(":");
-        var temp = array[4].substr(12, 4);
-
-        document.getElementsByClassName("templateStatusdaten")[0].append(speed[1] + " km");
-        document.getElementsByClassName("templateStatusdaten")[1].append(consumption[1] + " l");
-        document.getElementsByClassName("templateStatusdaten")[2].append(pressure + " Pa");
-        document.getElementsByClassName("templateStatusdaten")[3].append(temp + " °C");
-        document.getElementsByClassName("templateStatusdaten")[4].append(humidity[1] + " g/m³");
-    });
-});
-
-/*in Konsole hierfür:
-ssh pi@192.168.0.82
-ls 
-cd python
-ls 
-python3 services.py
-*   HIER ENDE KOMMTENTAR ENTFERNT!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-//Lock Unlock: Auto
-let ButtonZu = document.querySelector('.Verriegeln');
-function schliessenAuto() {
-    fetch("http://192.168.172.82:5000/action/lock").then(console.log("done"));
-}
-ButtonZu.addEventListener('click', schliessenAuto);
-
-let ButtonAuf = document.querySelector('.Aufschliessen');
-function oeffnenAuto() {
-    fetch("http://192.168.172.82:5000/action/unlock").then(console.log("done"));
-}
-ButtonAuf.addEventListener('click', oeffnenAuto);
-
-/*in Phython Services Datei ändern:
-- Unterfenster Konsole öffnen
-- ssh pi@192.168.0.82
-- ls 
-- cd python
-- ls 
-- nano services.py
---> dann öffnet sich die Datei und man kann bearbeiten
-*           HIER ENDE KOMMTENTAR ENTFERNT!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-//Lock Unlock: Fenster
-let ButtonAufFenster = document.querySelector('.Fenster');
-function oeffnenFenster() {
-    fetch("http://192.168.172.82:5000/window/open").then(console.log("done"));
-}
-ButtonAufFenster.addEventListener('click', oeffnenFenster); 
-
-let ButtonZuFenster = document.querySelector('.FensterZu');
-function schliessenFenster() {
-    fetch("http://192.168.172.82:5000/window/close").then(console.log("done"));
-}
-ButtonZuFenster.addEventListener('click', schliessenFenster);
-
-
-ENDE AUSKOMMENTIERT*/
-
-
 "use Strict";
 
 var IntervallWetter;
@@ -129,10 +57,10 @@ function handleHeaderClick(event) {
 
     switch (event.target.id) {
 
-        case 'home':
+        case 'homeID':
             ClickHome();
-        //    clearIntervall(IntervallWetter);
-        //    clearIntervall(IntervallAuto);
+            clearInterval(IntervallWetter);
+            clearInterval(IntervallAuto);
             break;
 
     }
@@ -144,16 +72,16 @@ function handleMainClick(event) {
 
         case 'StatusAutoID':
             ClickStatusAuto();
-   //         IntervallAuto = setIntervall(function() {
-   //             ClickStatusAuto();     
-	//		}, 1000);
+            IntervallAuto = setInterval(function () {
+                ClickStatusAuto();
+            }, 200000);
             break;
 
         case 'StatusWetterID':
             ClickStatusWetter();
-   //         IntervallWetter = setIntervall(function() {
-   //             ClickStatusWetter();     
-//			}, 1000);
+            IntervallWetter = setInterval(function () {
+                ClickStatusWetter();
+            }, 200000);
             break;
 
         case 'MusikID':
@@ -177,159 +105,161 @@ function handleMainClick(event) {
 //Klicken Statusinformation Auto
 function ClickStatusAuto() {
 
-  document.getElementById("menuContainer").style.display = "none";
+    let mainElement = document.querySelector('main');
+    mainElement.innerHTML = ' ';
 
-  var a = document.getElementById('AutoDatenScreen');
-  document.getElementsByTagName("main")[0].appendChild(a.content.cloneNode(true));
+    var a = document.getElementById('AutoDatenScreen');
+    document.getElementsByTagName("main")[0].appendChild(a.content.cloneNode(true));
 
 
-  "use Strict";
+    "use Strict";
 
-  fetch("http://192.168.178.61:5000/status").then(function (response) {         //192.168.0.82 im Labor!!     192.168.178.61
-    response.text().then(function (text) {
-        console.log(text);
-        var array = text.split(',');
-        var consumption = array[0].split(":");
-        var speed = array[3].split(":");
-     
-        document.getElementsByClassName("WertGeschwindigkeit")[0].append(speed[1] + " km/h");
-        document.getElementsByClassName("WertVerbrauch")[0].append(consumption[1] + " l");
+    fetch("http://192.168.0.82:5000/status").then(function (response) {
+        response.text().then(function (text) {
+            console.log(text);
+            var array = text.split(',');
+            var consumption = array[0].split(":");
+            var speed = array[3].split(":");
+
+            document.getElementsByClassName("WertGeschwindigkeit")[0].append(speed[1] + " km/h");
+            document.getElementsByClassName("WertVerbrauch")[0].append(consumption[1] + " l");
+        });
     });
-  });
 }
 
 //Klicken Statusinformation Wetter
 function ClickStatusWetter() {
 
-  document.getElementById("menuContainer").style.display = "none";
+    let mainElement = document.querySelector('main');
+    mainElement.innerHTML = ' ';
 
-  var w = document.getElementById('WetterDatenScreen');
-  document.getElementsByTagName("main")[0].appendChild(w.content.cloneNode(true));
+    var w = document.getElementById('WetterDatenScreen');
+    document.getElementsByTagName("main")[0].appendChild(w.content.cloneNode(true));
 
-  "use Strict";
+    "use Strict";
 
-  fetch("http://192.168.178.61:5000/status").then(function (response) {         //192.168.0.82 im Labor!!
-    response.text().then(function (text) {
-        console.log(text);
-        var array = text.split(',');
-        var humidity = array[1].substr(15, 6);
-        var pressure = array[2].substr(15, 6);
-        var temp = array[4].substr(12, 4);
+    fetch("http://192.168.0.82:5000/status").then(function (response) {
+        response.text().then(function (text) {
+            console.log(text);
+            var array = text.split(',');
+            var humidity = array[1].substr(15, 6);
+            var pressure = array[2].substr(15, 6);
+            var temp = array[4].substr(12, 4);
 
-        document.getElementsByClassName("WertDruck")[0].append(pressure + " Pa");
-        document.getElementsByClassName("WertTemperatur")[0].append(temp + " °C");
-        document.getElementsByClassName("WertLuftfeuchte")[0].append(humidity[1] + " g/m³");
+            document.getElementsByClassName("WertDruck")[0].append(pressure + " Pa");
+            document.getElementsByClassName("WertTemperatur")[0].append(temp + " °C");
+            document.getElementsByClassName("WertLuftfeuchte")[0].append(humidity[1] + " g/m³");
+        });
     });
-  });
 }
 
 //Klicken Musik
 function ClickMusik() {
-  document.getElementById("menuContainer").style.display = "none";
+    document.getElementById("menuContainer").style.display = "none";
 
-  var m = document.getElementById('MusikScreen');
-  document.getElementsByTagName("main")[0].appendChild(m.content.cloneNode(true));
+    var m = document.getElementById('MusikScreen');
+    document.getElementsByTagName("main")[0].appendChild(m.content.cloneNode(true));
 
-  "use Strict";
+    "use Strict";
 
-  fetch("http://192.168.178.61:5000/music").then(function (response) {
+    fetch("http://192.168.0.82:5000/music").then(function (response) {
         response.text().then(function (text) {
             tracks = JSON.parse(text);
-            console.log(text);  
+            console.log(text);
 
-  document.getElementsByClassName("MusikDaten")[0].append(tracks[0].artist, " - ", tracks[0].title);
-  document.getElementsByClassName("MusikDaten")[1].append(tracks[1].artist, " - ", tracks[1].title);
-  document.getElementsByClassName("MusikDaten")[2].append(tracks[2].artist, " - ", tracks[2].title);
-  });
-  });
+            document.getElementsByClassName("MusikDaten")[0].append(tracks[0].artist, " - ", tracks[0].title);
+            document.getElementsByClassName("MusikDaten")[1].append(tracks[1].artist, " - ", tracks[1].title);
+            document.getElementsByClassName("MusikDaten")[2].append(tracks[2].artist, " - ", tracks[2].title);
+        });
+    });
 
-  var song = document.getElementById("AudioID");
-  function PlayAudio() {
-    song.play()
-  }
-  function PauseAudio() {
-    song.pause()
-  }
-  document.querySelector(".Play").addEventListener('click', PlayAudio);
-  document.querySelector(".Pause").addEventListener('click', PauseAudio);
+    var song = document.getElementById("AudioID");
+    function PlayAudio() {
+        song.play()
+    }
+    function PauseAudio() {
+        song.pause()
+    }
+    document.querySelector(".Play").addEventListener('click', PlayAudio);
+    document.querySelector(".Pause").addEventListener('click', PauseAudio);
 }
 
 //Klicken Schloss
 function ClickSchloss() {
-  document.getElementById("menuContainer").style.display = "none";
+    document.getElementById("menuContainer").style.display = "none";
 
-  var s = document.getElementById('SchlossScreen');
-  document.getElementsByTagName("main")[0].appendChild(s.content.cloneNode(true));
+    var s = document.getElementById('SchlossScreen');
+    document.getElementsByTagName("main")[0].appendChild(s.content.cloneNode(true));
 
-  //Lock Unlock: Auto
-  "use Strict";
+    "use Strict";
+
     function schliessenAuto() {
-        fetch("http://192.168.178.61:5000/action/lock").then(console.log("done: Auto schließen"));
+        fetch("http://192.168.0.82:5000/action/lock").then(console.log("done: Auto schließen"));
     }
     document.querySelector('.Verriegeln').addEventListener('click', schliessenAuto);
 
     function oeffnenAuto() {
-        fetch("http://192.168.178.61:5000/action/unlock").then(console.log("done: Auto öffnen"));
+        fetch("http://192.168.0.82:5000/action/unlock").then(console.log("done: Auto öffnen"));
     }
     document.querySelector('.Aufschliessen').addEventListener('click', oeffnenAuto);
 }
 
 //Klicken Fenster
 function ClickFenster() {
-  document.getElementById("menuContainer").style.display = "none";
+    document.getElementById("menuContainer").style.display = "none";
 
-  var f = document.getElementById('FensterScreen');
-  document.getElementsByTagName("main")[0].appendChild(f.content.cloneNode(true));
+    var f = document.getElementById('FensterScreen');
+    document.getElementsByTagName("main")[0].appendChild(f.content.cloneNode(true));
 
-  "use Strict";
-  function oeffnenFensterLinks() {
-        fetch("http://192.168.178.61:5000/window/leftopen").then(console.log("done: Fenster öffnen"));
-  }
-  document.querySelector('.FensterLinksRunter').addEventListener('click', oeffnenFensterLinks); 
+    "use Strict";
+    function oeffnenFensterLinks() {
+        fetch("http://192.168.0.82:5000/window/leftopen").then(console.log("done: Fenster öffnen"));
+    }
+    document.querySelector('.FensterLinksRunter').addEventListener('click', oeffnenFensterLinks);
 
-  function schliessenFensterLinks() {
-        fetch("http://192.168.178.61:5000/window/leftclose").then(console.log("done: Fenster schließen"));
-  }
-  document.querySelector('.FensterLinksHoch').addEventListener('click', schliessenFensterLinks);
+    function schliessenFensterLinks() {
+        fetch("http://192.168.0.82:5000/window/leftclose").then(console.log("done: Fenster schließen"));
+    }
+    document.querySelector('.FensterLinksHoch').addEventListener('click', schliessenFensterLinks);
 
-  function oeffnenFensterRechts() {
-        fetch("http://192.168.178.61:5000/window/rightopen").then(console.log("done: Fenster öffnen"));
-  }
-  document.querySelector('.FensterRechtsRunter').addEventListener('click', oeffnenFensterRechts); 
+    function oeffnenFensterRechts() {
+        fetch("http://192.168.0.82:5000/window/rightopen").then(console.log("done: Fenster öffnen"));
+    }
+    document.querySelector('.FensterRechtsRunter').addEventListener('click', oeffnenFensterRechts);
 
-  function schliessenFensterRechts() {
-        fetch("http://192.168.178.61:5000/window/rightclose").then(console.log("done: Fenster schließen"));
-  }
-  document.querySelector('.FensterRechtsHoch').addEventListener('click', schliessenFensterRechts);
+    function schliessenFensterRechts() {
+        fetch("http://192.168.0.82:5000/window/rightclose").then(console.log("done: Fenster schließen"));
+    }
+    document.querySelector('.FensterRechtsHoch').addEventListener('click', schliessenFensterRechts);
 
-  function oeffnenFensterAlle() {
-        fetch("http://192.168.178.61:5000/window/allopen").then(console.log("done: Fenster öffnen"));
-  }
-  document.querySelector('.FensterAlleRunter').addEventListener('click', oeffnenFensterAlle); 
+    function oeffnenFensterAlle() {
+        fetch("http://192.168.0.82:5000/window/allopen").then(console.log("done: Fenster öffnen"));
+    }
+    document.querySelector('.FensterAlleRunter').addEventListener('click', oeffnenFensterAlle);
 
-  function schliessenFensterAlle() {
-        fetch("http://192.168.178.61:5000/window/allclose").then(console.log("done: Fenster schließen"));
-  }
-  document.querySelector('.FensterAlleHoch').addEventListener('click', schliessenFensterAlle);
+    function schliessenFensterAlle() {
+        fetch("http://192.168.0.82:5000/window/allclose").then(console.log("done: Fenster schließen"));
+    }
+    document.querySelector('.FensterAlleHoch').addEventListener('click', schliessenFensterAlle);
 }
 
 //Klicken Navigation
 function ClickNavi() {
-  document.getElementById("menuContainer").style.display = "none";
+    document.getElementById("menuContainer").style.display = "none";
 
-  var n = document.getElementById('NaviScreen');
-  document.getElementsByTagName("main")[0].appendChild(n.content.cloneNode(true));
+    var n = document.getElementById('NaviScreen');
+    document.getElementsByTagName("main")[0].appendChild(n.content.cloneNode(true));
 }
 
 //Klick Home
 function ClickHome() {
-  let mainElement = document.querySelector('main');
-  mainElement.innerHTML = '';
-  
-  var h = document.getElementById('HomeScreen');
-  document.getElementsByTagName("main")[0].appendChild(h.content.cloneNode(true));
+    let mainElement = document.querySelector('main');
+    mainElement.innerHTML = '';
 
- // fetch("http://192.168.178.61:5000/action/black").then(console.log("done: Fenster schließen"));
+    var h = document.getElementById('HomeScreen');
+    document.getElementsByTagName("main")[0].appendChild(h.content.cloneNode(true));
+
+    fetch("http://192.168.0.82:5000/window/black").then(console.log("done: LED's aus"));
 }
 
 document.querySelector('main').addEventListener('click', handleMainClick);
